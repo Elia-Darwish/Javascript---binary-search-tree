aed3acb933ca200d1680e4a35e078f32e34e8020
Elia Darwish <darwish.elia@gmail.com>
2018-08-23 12:17:36 +0200

    object has been abstracted

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -5,112 +5,142 @@
 
 
 //Binary Search Tree
+(function (global) {
+
+    // tree inorder traversal --- recursively navigating the tree ( left child --> parent --> right child)
+    let inOrder = function (obj, arr) {
 
 
-function Node(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-}
+        if (obj.left) {
+            inOrder(obj.left, arr);
+        }
+        arr.push(obj.value);
 
-
-
-Node.prototype.insert = function (value) {
-    if (value > this.value) {
-
-        if (!this.right) {
-            this.right = new Node(value);
-        } else {
-            this.right.insert(value);
+        if (obj.right) {
+            inOrder(obj.right, arr);
         }
 
-
-    } else if (value < this.value) {
-
-        if (!this.left) {
-            this.left = new Node(value);
-        } else {
-            this.left.insert(value);
-        }
-
-    } else {
-        return;
     }
-}
 
 
+    // tree preorder traversal --- recursively navigating the tree ( parent --> left child --> right child)
+    let preOrder = function (obj, arr) {
 
-Node.prototype.hasValue = function (value) {
+        arr.push(obj.value);
 
-    if (value === this.value) {
-        return true;
-    } else {
+        if (obj.left) {
+            preOrder(obj.left, arr);
+        }
 
-        if (value > this.value) {
-            if (this.right) {
-                return this.right.hasValue(value);
+        if (obj.right) {
+            preOrder(obj.right, arr);
+        }
+
+    }
+
+
+    // tree Postorder traversal --- recursively navigating the tree ( left child --> right child --> parent)
+    let postOrder = function (obj, arr) {
+
+
+        if (obj.left) {
+            postOrder(obj.left, arr);
+        }
+
+        if (obj.right) {
+            postOrder(obj.right, arr);
+        }
+
+        arr.push(obj.value);
+
+    }
+
+    // the node object
+    function Node(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    // adding insert, search and sort methods to the node object
+    Node.prototype = {
+
+        insert: function (value) {
+            if (value > this.value) {
+
+                if (!this.right) {
+                    this.right = new Node(value);
+                } else {
+                    this.right.insert(value);
+                }
+
+
+            } else if (value < this.value) {
+
+                if (!this.left) {
+                    this.left = new Node(value);
+                } else {
+                    this.left.insert(value);
+                }
+
             } else {
-                return false;
+                return;
             }
-        }
+        },
 
-        if (value < this.value) {
-            if (this.left) {
-                return this.left.hasValue(value);
+
+        hasValue: function (value) {
+
+            if (value === this.value) {
+                return true;
             } else {
-                return false;
+
+                if (value > this.value) {
+                    if (this.right) {
+                        return this.right.hasValue(value);
+                    } else {
+                        return false;
+                    }
+                }
+
+                if (value < this.value) {
+                    if (this.left) {
+                        return this.left.hasValue(value);
+                    } else {
+                        return false;
+                    }
+                }
+
             }
+
+        },
+
+
+        getInOrder: function () {
+            let arr = [];
+            inOrder(this, arr);
+            return arr;
+        },
+
+        getPreOrder: function () {
+            let arr = [];
+            preOrder(this, arr);
+            return arr;
+        },
+
+        getPostOrder: function () {
+            let arr = [];
+            postOrder(this, arr);
+            return arr;
         }
 
-    }
 
-}
-
+    };
 
 
-let inOrder = function (obj, arr) {
-
-
-    if (obj.left) {
-        inOrder(obj.left, arr);
-    }
-    arr.push(obj.value);
-
-    if (obj.right) {
-        inOrder(obj.right, arr);
-    }
-
-}
-
-
-Node.prototype.getInOrder = function () {
-    let arr = [];
-    inOrder(this, arr);
-    return arr;
-}
+    global.Node = Node;
 
 
 
-
-
-
-// let BST = new Node(20);
-
-// BST.insert(17);
-// BST.insert(2);
-// BST.insert(34);
-// BST.insert(19);
-// BST.insert(12);
-// BST.insert(45);
-// BST.insert(22);
-// BST.insert(15);
-// BST.insert(6);
-// BST.insert(28);
-// BST.insert(50);
-// BST.insert(1);
-// BST.insert(12);
-
-
-// console.log(BST);
-// console.log(BST.inOrderGet());
-// console.log(BST.hasValue(34));
+}(window));
